5f4d97b3f2797016fde0d88a22c594e75eb8db18
Antony <antgrachev2312@gmail.com>
2024-10-24 11:58:21 +0300

    add shop card

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FaShoppingCart } from "react-icons/fa";
+import Order from "./Order";
 
-export default function Header() {
+const showOrders=(props) => {
+    return (
+        <div>
+            {props.orders.map(el => (
+                <Order key={el.id} item={el}/>
+            ))}
+        </div>
+    )
+}
+
+const showNothing = () => {
+    return (
+        <div className='empty'>
+            <h2>Товаров нет</h2>
+        </div>
+    )
+}
+
+export default function Header(props) {
+
+    let [cartOpen, setCartOpen] =useState(false)
+
     return (
         <header>
             <div className='header-content'>
                 <span className='logo'>Магазин</span>
                 <ul className='nav'>
-                    <FaShoppingCart className='shop-card-button' />
+                    <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-card-button ${cartOpen && 'active'}`}/>
+                    {cartOpen && (
+                        <div className='shop-card'>
+                            {props.orders.length > 0 ?
+                            showOrders(props) : showNothing()}
+
+                        </div>
+                    )}
                     <li>О нас</li>
                     <li>Контакты</li>
                     <li>Кабинет</li>
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-export class Item extends Component {
+export class Order extends Component {
     render() {
         return (
             <div className='item'>
                 <img src={"./img/" + this.props.item.img}></img>
                 <h2>{this.props.item.title}</h2>
-                <p>{this.props.item.desc}</p>
                 <b>{this.props.item.price}₽</b>
-                <div className='add-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
             </div>
         )
+
     }
 }
 
-export default Item
+export default Order
